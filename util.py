f43a0be2b34d73ac7b28a96955919b830bd06dd1
Kenneth Hanson <khanson679@gmail.com>
2015-06-28 15:52:02 +0900

    Add "warning" logging function.

--- util.py
+++ util.py
@@ -1,6 +1,12 @@
 """
 Utility functions for lingbib app.
+
+Logging functions currently use the print statement. In the future, they will
+use the Python logging system.
 """
 
 def debug(obj):
     print("DEBUG: " + str(obj))
+
+def warning(msg):
+    print("WARNING: " + msg)
