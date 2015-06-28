e8cfce97a096e4e9b1892e7312dd826c5969605a
Kenneth Hanson <khanson679@gmail.com>
2015-06-28 16:17:34 +0900

    Added "info" and "error" to utility functions.
    Calls to "error" utility function end the program.

--- util.py
+++ util.py
@@ -8,5 +8,12 @@ use the Python logging system.
 def debug(obj):
     print("DEBUG: " + str(obj))
 
+def info(msg):
+    print("INFO: " + msg)
+
 def warning(msg):
     print("WARNING: " + msg)
+
+def error(msg):
+    print("Error: " + msg)
+    exit(1)
