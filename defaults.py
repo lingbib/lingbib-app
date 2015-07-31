56d5ca60c7b6ba9abd0334eb93d19ed4ff83a0a0
Kenneth Hanson <khanson679@gmail.com>
2015-07-31 11:38:35 +0900

    Added more symbolic constants.

--- defaults.py
+++ defaults.py
@@ -1,3 +1,13 @@
 DB_MASTER = "lingbib-master.bib"
 DB_BIBTEX = "lingbib-bibtex.bib"
 DB_PERSONAL = "lingbib-personal.bib"
+
+BRANCH_MASTER = "master"
+BRANCH_DBEDIT = "dbedit"
+
+REMOTE_LINGBIB = "lingbib"
+REMOTE_PERSONAL = "personal"
+
+REMOTE_LINGBIB_MASTER = "lingbib/master"
+REMOTE_PERSONAL_MASTER = "personal/master"
+REMOTE_PERSONAL_DBEDIT = "personal/dbedit"
